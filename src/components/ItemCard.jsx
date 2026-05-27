a4d47ce3d71519f897c79e7c51bc0c5092eb8efa
Chinonye <faithchinonye53@gmailcom>
2026-05-28 00:28:46 +0200

    implement some styles

--- src/components/ItemCard.jsx
+++ src/components/ItemCard.jsx
@@ -1,37 +1,39 @@
 import React from "react";
 import { useState } from "react";
 import list from "../assets/list.json";
-import "./itemCard.css";
 
 export default function ItemCard() {
   const [apartmentList, setApartmentList] = useState(list.results);
-  console.log(apartmentList)
+  console.log(apartmentList);
 
   function handleDeleteItem(id) {
-    setApartmentList(apartmentList.filter(element=> element.id !== id));
+    setApartmentList(apartmentList.filter((element) => element.id !== id));
   }
 
   return (
-    <div>
+    <div className="card">
       {apartmentList.map((element) => {
         return (
           <div>
             {element.has_availability === true ? (
-              <article className="card">
-                <header>
-                  <h2>{element.name}</h2>
-                </header>
+              <div>
+                <h2>{element.name}</h2>
 
                 <img src={element.picture_url} alt="" />
-                <p></p>
+
                 <h4>{element.host_location}</h4>
 
                 <div className="content">
                   <p>{element.description}</p>
                 </div>
-                <button onClick={()=>{handleDeleteItem(element.id)}}>delete</button>
-                <button>{}</button>
-              </article>
+                <button
+                  onClick={() => {
+                    handleDeleteItem(element.id);
+                  }}
+                >
+                  delete
+                </button>
+              </div>
             ) : null}
           </div>
         );
